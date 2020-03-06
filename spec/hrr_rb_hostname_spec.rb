cd2cef3a153fdfafc839c1d5748f692ee445bcc2
hirura <hirura@gmail.com>
2020-03-06 20:47:23 +0900

    update spec file

--- spec/hrr_rb_hostname_spec.rb
+++ spec/hrr_rb_hostname_spec.rb
@@ -3,7 +3,49 @@ RSpec.describe HrrRbHostname do
     expect(HrrRbHostname::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe ".get" do
+    it "returns hostname" do
+      expect(HrrRbHostname.get).to eq `hostname`.chomp
+    end
+  end
+
+  describe ".gethostname" do
+    it "returns hostname" do
+      expect(HrrRbHostname.gethostname).to eq `hostname`.chomp
+    end
+  end
+
+  describe ".set" do
+    before :example do
+      @name_bak = `hostname`.chomp
+    end
+
+    after :example do
+      `hostname "#{@name_bak}"`
+    end
+
+    let(:name){ "new#{@name_bak}" }
+
+    it "sets hostname and returns it" do
+      expect(HrrRbHostname.set(name)).to eq name
+      expect(`hostname`.chomp).to eq name
+    end
+  end
+
+  describe ".sethostname" do
+    before :example do
+      @name_bak = `hostname`.chomp
+    end
+
+    after :example do
+      `hostname "#{@name_bak}"`
+    end
+
+    let(:name){ "new#{@name_bak}" }
+
+    it "sets hostname and returns it" do
+      expect(HrrRbHostname.sethostname(name)).to eq name
+      expect(`hostname`.chomp).to eq name
+    end
   end
 end
